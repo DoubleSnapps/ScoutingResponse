399aabd02845dffb2bcba7238565dfe942a6a01f
Natalie <64873273+DoubleSnapps@users.noreply.github.com>
2022-11-04 09:32:31 -0500

    ok i fixed it
    i love hotstops i love hotstops i love hotsops

---
+++ app/src/main/java/com/example/snsresponseapplication5431/Teleop.kt
@@ -0,0 +1,16 @@
+package com.example.snsresponseapplication5431
+
+import androidx.fragment.app.Fragment
+
+class Teleop {
+public Int = roboNumber;
+
+     class Teleop : Fragment() {
+
+
+
+
+
+
+    }
+}
