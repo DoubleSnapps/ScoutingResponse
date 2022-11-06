a1c1bfcae5f0f940ca27e2a3d92ad2e847d19745
Natalie <64873273+DoubleSnapps@users.noreply.github.com>
2022-11-06 00:25:29 -0500

    Finished basic button string

--- app/src/main/java/com/example/snsresponseapplication5431/Prematch.kt
+++ app/src/main/java/com/example/snsresponseapplication5431/Prematch.kt
@@ -6,16 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.snsresponseapplication5431.databinding.PrematchFragmentBinding
+import com.example.snsresponseapplication5431.databinding.FragmentFirstBinding
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * Prematch Fragment.
  */
 class Prematch : Fragment() {
 
-    private var _binding: PrematchFragmentBinding? = null
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentFirstBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -23,7 +21,7 @@ class Prematch : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = PrematchFragmentBinding.inflate(inflater, container, false)
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
 
     }
