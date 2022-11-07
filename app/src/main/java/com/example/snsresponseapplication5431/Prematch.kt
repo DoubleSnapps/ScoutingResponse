5afac7d0ac399b522072346234f41aa9d08bdbb6
Natalie <64873273+DoubleSnapps@users.noreply.github.com>
2022-11-07 14:07:09 -0600

    I made something of worth

--- app/src/main/java/com/example/snsresponseapplication5431/Prematch.kt
+++ app/src/main/java/com/example/snsresponseapplication5431/Prematch.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import androidx.navigation.fragment.findNavController
 import com.example.snsresponseapplication5431.databinding.FragmentFirstBinding
 
@@ -12,10 +13,9 @@ import com.example.snsresponseapplication5431.databinding.FragmentFirstBinding
  * Prematch Fragment.
  */
 class Prematch : Fragment() {
-
+    public var teamThere = false
     private var _binding: FragmentFirstBinding? = null
     private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,21 +25,25 @@ class Prematch : Fragment() {
         return binding.root
 
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
-    }
+        // is the team showed check box clicked?
+        var button = view.findViewById<CheckBox>(R.id.didTeamShow)
+        button.setOnClickListener {
+            // if so, set teamThere to true
+            if(button.isChecked) {
+                var teamThere = true;
 
+            }
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-
-
-
-
     }
 }
+
